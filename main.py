2892f969f811f884ea0c70b1d710a3f865ca7b3a
Tung Nguyen <tung_nguyen2@epam.com>
2024-06-28 16:50:48 +0700

    (feat) add streamlit, show step by step asynchronously running on Chat Streamlit

--- main.py
+++ main.py
@@ -1,29 +1,57 @@
-from openai import AzureOpenAI
+import asyncio
+import streamlit as st
+from streamlit_chat import message
+from langchain_openai import ChatOpenAI
 
-client = AzureOpenAI(
-    api_key="",  # leave your key here
-    api_version="",
-    azure_endpoint=""
-)
-
-deployment_name = "gpt-35-turbo"
-
-# Please only use this one if you absolutely need it. It's slower and more expensive.
-# deployment_name = "gpt-4"
-# deployment_name = "gpt-4-32k"
-
-# For embeddings only, but small private models may perform better and cheaper
-# https://huggingface.co/spaces/mteb/leaderboard
-# deployment_name = "text-embedding-ada-002"
+from exploiter.exploit import exploit_cve
 
 
-print(client.chat.completions.create(
-    model=deployment_name,
-    temperature=0,
-    messages=[
-        {
-            "role": "user",
-            "content": "how are you?",
-        },
-    ],
-))
+user_prompt = "user_prompt_history"
+chat_answers = "chat_answers_history"
+chat_history = "chat_history"
+
+llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0.5)
+
+async def main():
+    st.header("Langchain Documentation Helper Bot")
+
+    with st.form(key="Prompt", clear_on_submit=False):
+        prompt = st.text_input("Enter your prompt here")
+        submitted = st.form_submit_button("Submit")
+
+    if submitted:
+        cve_name = llm.invoke(f"Extract cve_name from following request: {prompt}.\
+                                Example output: 'CVE-211-1234'").content
+
+        st.write(f"Extracted CVE name: {cve_name}")
+        message(f"Extracted CVE name: {cve_name}")
+
+        def write_message(key, text, is_user=False, idx=None):
+            message(f"{key}: {text}", is_user=is_user,key=idx)
+
+        if prompt:
+            with st.spinner("Generating response"):
+                router_agent_executor, input_dict = exploit_cve(cve_name)
+                print(input_dict)
+                idx = 0
+                async for chunk in router_agent_executor.astream(input_dict):
+                    try:
+                        idx += 1
+                        # Agent Action
+                        if "actions" in chunk:
+                            for action in chunk["actions"]:
+                                write_message("Calling Tool", f"`{action.tool}` with input `{action.tool_input}`", idx=idx)
+                        # Observation
+                        elif "steps" in chunk:
+                            for step in chunk["steps"]:
+                                write_message("Tool Result", f"`{step.observation}`", is_user=True, idx=idx)
+                        # Final result
+                        elif "output" in chunk:
+                            write_message('Final Output', f'{chunk["output"]}', is_user=True, idx=idx)
+                        else:
+                            raise ValueError()
+                    except Exception as e:
+                        pass
+
+if __name__ == "__main__":
+    asyncio.run(main())
